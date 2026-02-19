b3e552c5dae881eff9cfb3ff91980970583bab27
aidanj2m <aidanj2m@gmail.com>
2026-02-19 10:57:30 -0500

    Initial Commit

---
+++ Spare Time/Spare_TimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spare_TimeApp.swift
+//  Spare Time
+//
+//  Created by Aidan McKenzie on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct Spare_TimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Spare TimeTests/Spare_TimeTests.swift
@@ -0,0 +1,16 @@
+//
+//  Spare_TimeTests.swift
+//  Spare TimeTests
+//
+//  Created by Aidan McKenzie on 2/19/26.
+//
+
+import Testing
+
+struct Spare_TimeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
